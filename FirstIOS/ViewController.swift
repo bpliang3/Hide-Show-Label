86f0b7de80dbdb0f074dd2b99deefa0e121d7616
Ben Liang <bpliang3@illinois.edu>
2015-11-12 12:50:18 -0600

    Simple App with buttons and label through storyboard interface

--- FirstIOS/ViewController.swift
+++ FirstIOS/ViewController.swift
@@ -9,8 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var hide: UIButton!
+    @IBOutlet weak var show: UIButton!
+    @IBOutlet weak var label: UILabel!
+    
+    
+    @IBAction func showAction(sender: UIButton) {
+        label.hidden = false
+        
+    }
+    
+    @IBAction func hideAction(sender: UIButton) {
+        label.hidden = true
+    }
 
     override func viewDidLoad() {
+        
+        label.hidden = true
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
